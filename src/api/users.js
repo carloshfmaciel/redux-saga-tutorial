456a1240f8fe2c5183631bec5834d4efc27e7f47
Carlos Maciel <carloshfmaciel@MacBook-Pro-de-Carlos.local>
2019-05-12 02:33:06 -0300

    dispatching getUsers at start of App

--- src/api/users.js
+++ src/api/users.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getUsers = () => {
-  return axios.get('/users', {
+  return axios.get('http://rem-rest-api.herokuapp.com/api/users', {
     params: {
       limit: 1000
     }
